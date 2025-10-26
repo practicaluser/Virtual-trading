e722796a132ddf48480ac4d283317650817d08df
practicaluser <dlcnsdncjswo@naver.com>
2025-10-26 17:36:42 +0900

    Allowed Host 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-1)wkavcw(2f5a9wzxfb^p%_wc4nds*w!+%)haja*0qo3-pkac0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.164.164.166']
+ALLOWED_HOSTS = ["15.164.164.166"]
 
 
 # Application definition
