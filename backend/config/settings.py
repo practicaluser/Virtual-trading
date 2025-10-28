b46477dc6dbd12b2b4920df88ae7832bd132db80
practicaluser <dlcnsdncjswo@naver.com>
2025-10-28 09:53:39 +0900

    세팅 수정1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -247,6 +247,11 @@ LOGGING = {
 # 메시지 브로커 URL (Docker Compose의 Redis 서비스 이름 사용)
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 CELERY_BROKER_URL = f"redis://{REDIS_HOST}:6379/0"
+
+
+# [추가] 🔽 Celery 시작 시 이 값이 로그에 찍히도록 함
+print(f"DEBUG: CELERY_BROKER_URL is set to: {CELERY_BROKER_URL}")
+
 # 작업 결과 백엔드 URL
 CELERY_RESULT_BACKEND = "django-db"  # django-celery-results 사용 (DB에 결과 저장)
 # 작업 결과가 JSON으로 직렬화되도록 설정 (기본값)
